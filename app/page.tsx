9a832a778b4c5aec3e24916e403b8fbb5cf82976
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-12 19:32:16 +0530

    [feat]: Add Reputation Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import OurReputation from "@/components/OurReputation";
 
 const page = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <OurReputation />
     </div>
   );
 };
---
+++ components/OurReputation.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+const OurReputation = () => {
+  return (
+    <div className="max-w-6xl mx-auto my-40 space-y-28">
+      <h1 className="text-center font-semibold text-3xl text-secondary">
+        Our Reputation
+      </h1>
+      <div className="flex justify-between">
+        <div className="space-y-2 py-4 px-3 border-2 border-[#E0E3EB]">
+          <Image src="/icon1.png" width={30} height={30} alt="icon" />
+          <h1 className="font-semibold text-lg text-secondary">
+            Best Services
+          </h1>
+          <p className="text-gray text-sm max-w-[16rem]">
+            Nullam senectus porttitor in eget. Eget rutrum leo interdum.
+          </p>
+        </div>
+        <div className="space-y-2 py-4 px-3 border-2 border-[#E0E3EB]">
+          <Image src="/icon1.png" width={30} height={30} alt="icon" />
+          <h1 className="font-semibold text-lg text-secondary">
+            Best Services
+          </h1>
+          <p className="text-gray text-sm max-w-[16rem]">
+            Cursus semper tellus volutpat aliquet lacus.
+          </p>
+        </div>
+        <div className="space-y-2 py-4 px-3 border-2 border-[#E0E3EB]">
+          <Image src="/icon2.png" width={30} height={30} alt="icon" />
+          <h1 className="font-semibold text-lg text-secondary">
+            Best Services
+          </h1>
+          <p className="text-gray text-sm max-w-[16rem]">
+            Ultricies at ipsum nunc, tristique nam lectus.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default OurReputation;
