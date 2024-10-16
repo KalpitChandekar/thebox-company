87acf12b5b5a79aac867e50e387b3715279f12c2
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-16 09:17:15 +0530

    update UI

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,15 +1,15 @@
 const Contact = () => {
   return (
     <div className="max-w-7xl bg-[#F6F8F7] my-40 mx-auto">
-      <div className="max-w-3xl mx-auto py-10 space-y-2">
+      <div className="max-w-md text-center mx-auto py-10 space-y-2">
         <h1 className="text-center font-semibold text-3xl text-secondary">
-          Services
+          What can us do for you?
         </h1>
-        <p className="max-2xl:">
+        <p className="leading-5">
           We are ready to work on a project of any complexity, whether it’s
           commercial or residential.
         </p>
-        <button className="py-2 px-6 bg-blue text-white">Submit</button>
+        <button className="py-2 px-20 rounded-sm bg-blue text-white">Submit</button>
       </div>
     </div>
   );
