63cc43b349a7dc6ede92e6b63923bd7bf959dd9d
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-16 09:09:50 +0530

    style contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,4 +1,17 @@
 const Contact = () => {
-  return <div className="max-w-6xl mx-auto">Contact</div>;
+  return (
+    <div className="max-w-7xl bg-[#F6F8F7] my-40 mx-auto">
+      <div className="max-w-3xl mx-auto py-10 space-y-2">
+        <h1 className="text-center font-semibold text-3xl text-secondary">
+          Services
+        </h1>
+        <p className="max-2xl:">
+          We are ready to work on a project of any complexity, whether it’s
+          commercial or residential.
+        </p>
+        <button className="py-2 px-6 bg-blue text-white">Submit</button>
+      </div>
+    </div>
+  );
 };
 export default Contact;
