42e91323a5cc585a22548bd11992600a16c9642e
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-19 20:20:10 +0530

    Make Footer Responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,20 +3,35 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className="max-w-7xl mx-auto">
-      <div className="flex text-secondary max-w-5xl mx-auto justify-between py-14">
+      <div className="flex md:flex-row flex-col md:px-0 px-4 text-secondary max-w-5xl mx-auto justify-between py-14 gap-16">
         <div className="space-y-6">
-          <div className="flex gap-2">
-            <div className="space-y-4 text-blue font-medium">
-              <p>ADDRESS:</p>
-              <p>PHONE:</p>
-              <p>EMAIL:</p>
-            </div>
-            <div className="space-y-4">
-              <p>6391 Elgin St. Celina, Delaware 10299</p>
-              <p>+1-543-123-4567</p>
-              <p>hello@thebox.com</p>
-            </div>
+          <div className="overflow-x-auto">
+            <table className="table-auto w-full text-left">
+              <tbody>
+                <tr>
+                  <td className="font-medium align-top text-blue px-2 py-2">
+                    ADDRESS:
+                  </td>
+                  <td className="px-2 py-2">
+                    6391 Elgin St. Celina, Delaware 10299
+                  </td>
+                </tr>
+                <tr>
+                  <td className="font-medium align-top text-blue px-2 py-2">
+                    PHONE:
+                  </td>
+                  <td className="px-2 py-2">+1-543-123-4567</td>
+                </tr>
+                <tr>
+                  <td className="font-medium align-top text-blue px-2 py-2">
+                    EMAIL:
+                  </td>
+                  <td className="px-2 py-2">hello@thebox.com</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+
           <div className="flex gap-1 items-center cursor-pointer">
             <Image src="/symbol.png" alt="logo" width={29} height={36} />
             <Image src="/box.png" alt="logo" width={80} height={17} />
@@ -49,7 +64,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="bg-blue py-4">
+      <div className="bg-blue text-center md:text-start py-4">
         <p className="text-white text-sm max-w-5xl mx-auto">
           TheBox Company © 2024. All Rights Reserved
         </p>
