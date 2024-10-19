9950f22d9aa06bdb92685ed06facc514f71e4e37
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-19 20:11:48 +0530

    make navbar responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,14 @@
+import { Menu } from "lucide-react";
 import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center max-w-6xl mx-auto my-2">
+    <div className="px-4 md:px-0 flex justify-between items-center max-w-6xl mx-auto my-2">
       <div className="flex gap-1 items-center cursor-pointer">
         <Image src="/symbol.png" alt="logo" width={29} height={36} />
         <Image src="/box.png" alt="logo" width={80} height={17} />
       </div>
-      <ul>
+      <ul className="hidden md:flex">
         <li className="inline-block mx-3 cursor-pointer font-medium">Home</li>
         <li className="inline-block mx-3 cursor-pointer font-medium">About Us</li>
         <li className="inline-block mx-3 cursor-pointer font-medium">Projects</li>
@@ -16,6 +17,7 @@ const Navbar = () => {
           Contact Us
         </li>
       </ul>
+      <Menu className="md:hidden" size={32} />
     </div>
   );
 };
